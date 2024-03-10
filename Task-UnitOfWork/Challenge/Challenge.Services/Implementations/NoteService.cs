21b91c7447887fef3a7be97b160e4e4226836eea
simon-k1993 <kozoloski_simon@yahoo.com>
2024-03-10 23:25:17 +0100

    Task-UnitOfWork

---
+++ Task-UnitOfWork/Challenge/Challenge.Services/Implementations/NoteService.cs
@@ -0,0 +1,112 @@
+﻿using Challenge.DataAccess.Interfaces;
+using Challenge.Domain.Entities;
+using Challenge.DTOs;
+using Challenge.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge.Services.Implementations
+{
+    public class NoteService : INoteService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public NoteService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task AddNote(NoteAddDTO addNoteDto)
+        {
+            if (addNoteDto == null)
+            {
+                throw new Exception("Entity cannot be null");
+            }
+
+            var noteToAdd = new Note
+            {
+                Name = addNoteDto.Name,
+                DueDate = addNoteDto.DueDate,
+                Description = addNoteDto.Description,
+                Status = addNoteDto.Status
+            };
+
+            _unitOfWork.NoteRepository.Add(noteToAdd);
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task DeleteNote(int id)
+        {
+            var note = _unitOfWork.NoteRepository.GetById(id);
+
+            if (note == null)
+            {
+                throw new Exception("Entity cannot be null");
+            }
+
+            _unitOfWork.NoteRepository.Delete(note);
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task<List<NoteToReturnDTO>> GetAllNotes()
+        {
+            var notes = _unitOfWork.NoteRepository.GetAll();
+
+            return notes.Select(note => new NoteToReturnDTO
+            {
+                Id = note.Id,
+                Name = note.Name,
+                DueDate = note.DueDate,
+                Description = note.Description,
+                Status = note.Status.ToString()
+            }).ToList();
+        }
+
+        public async Task<NoteToReturnDTO> GetById(int id)
+        {
+            var note = _unitOfWork.NoteRepository.GetById(id);
+
+            if (note == null)
+            {
+                throw new Exception("Entity cannot be null");
+            }
+
+            return new NoteToReturnDTO
+            {
+                Id = note.Id,
+                Name = note.Name,
+                DueDate = note.DueDate,
+                Description = note.Description,
+                Status = note.Status.ToString()
+            };
+        }
+
+        public async Task UpdateNote(int id, NoteUpdatedDTO updateNoteDto)
+        {
+            if (updateNoteDto == null)
+            {
+                throw new Exception("Entity cannot be null");
+            }
+
+            var existingNote = _unitOfWork.NoteRepository.GetById(id);
+
+            if (existingNote == null)
+            {
+                throw new Exception("Entity cannot be null");
+            }
+
+            existingNote.Name = updateNoteDto.Name;
+            existingNote.DueDate = updateNoteDto.DueDate;
+            existingNote.Description = updateNoteDto.Description;
+            existingNote.Status = updateNoteDto.Status;
+
+            _unitOfWork.NoteRepository.Update(existingNote);
+            await _unitOfWork.SaveChangesAsync();
+        }
+    }
+
+}
+
---
+++ Task-UnitOfWork/Challenge/Challenge/Program.cs
@@ -0,0 +1,38 @@
+using Challenge.DataAccess.Implementations;
+using Challenge.DataAccess.Interfaces;
+using Challenge.DataAccess;
+using Challenge.Domain.Entities;
+using Challenge.Services.Implementations;
+using Challenge.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<INoteRepository<Note>, NoteRepository>();
+builder.Services.AddScoped<INoteService, NoteService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
